96f2482ff66f825e3e2687a4151f7b6b76e45490
DevanFarmer <devanf21@gmail.com>
2024-10-15 16:52:18 +0200

    Manage Locations
    Untested

--- Fault Subsystem/FaultSubsystem/FaultSubsystem/Controllers/AdministratorController.cs
+++ Fault Subsystem/FaultSubsystem/FaultSubsystem/Controllers/AdministratorController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using FaultSubsystem.Models.Shared;
 using Newtonsoft.Json;
+using FaultSubsystem.Data;
+using FaultSubsystem.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FaultSubsystem.Controllers
 {
     public class AdministratorController : Controller
     {
+        private readonly ApplicationDbContext _dBContext;
+
+        public AdministratorController(ApplicationDbContext context)
+        {
+            _dBContext = context;
+        }
+
         [HttpGet]
         public IActionResult Dashboard()
         {
@@ -25,5 +35,64 @@ namespace FaultSubsystem.Controllers
         {
             return RedirectToAction("ViewRoles", "Role");
         }
+
+        #region Manage Locations
+        // View all locations
+        public async Task<IActionResult> ViewLocations()
+        {
+            var locations = await _dBContext.Location.ToListAsync();
+            return View(locations);
+        }
+
+        // GET: Add location
+        public IActionResult AddLocation()
+        {
+            return View();
+        }
+
+        // POST: Add location
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddLocation(Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                _dBContext.Location.Add(location);
+                await _dBContext.SaveChangesAsync();
+                return RedirectToAction(nameof(ViewLocations));
+            }
+            return View(location);
+        }
+
+        // GET: Edit location
+        public async Task<IActionResult> EditLocation(int id)
+        {
+            var location = await _dBContext.Location.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            return View(location);
+        }
+
+        // POST: Edit location
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditLocation(int id, Location location)
+        {
+            if (id != location.LocationID)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _dBContext.Location.Update(location);
+                await _dBContext.SaveChangesAsync();
+                return RedirectToAction(nameof(ViewLocations));
+            }
+            return View(location);
+        }
+        #endregion
     }
 }
