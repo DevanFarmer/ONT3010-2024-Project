1504f53ad5098e92e8a995a55c045f06f3593d26
DevanFarmer <devanf21@gmail.com>
2024-10-20 14:59:51 +0200

    Inventory Liaison Validation
    Untested

--- Fault Subsystem/FaultSubsystem/FaultSubsystem/Models/Inventory.cs
+++ Fault Subsystem/FaultSubsystem/FaultSubsystem/Models/Inventory.cs
@@ -5,8 +5,16 @@ namespace FaultSubsystem.Models
     public class Inventory
     {
         public int FridgeTypeID { get; set; }
+
+        [Required(ErrorMessage = "Please select a Supplier")]
         public int SupplierID { get; set; }
+
+        [Required(ErrorMessage = "A Model Name is required")]
+        [StringLength(55, MinimumLength = 3, ErrorMessage = "First Name must be between 3 and 55 characters")]
         public string FridgeModel { get; set; }
+
+        [Required(ErrorMessage = "A quantity is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter a valid integer. Quantity cannot be lower than 0")]
         public int Quantity { get; set; }
 
         public Supplier Supplier { get; set; }
--- Fault Subsystem/FaultSubsystem/FaultSubsystem/Models/Supplier.cs
+++ Fault Subsystem/FaultSubsystem/FaultSubsystem/Models/Supplier.cs
@@ -1,9 +1,17 @@
-﻿namespace FaultSubsystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FaultSubsystem.Models
 {
     public class Supplier
     {
         public int SupplierID { get; set; }
+
+        [Required(ErrorMessage = "Supplier Name is required")]
+        [StringLength(55, MinimumLength = 3, ErrorMessage = "First Name must be between 3 and 55 characters")]
         public string SupplierName { get; set;}
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
         public string SupplierPhoneNumber { get; set;}
     }
 }
