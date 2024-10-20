883dcec58c0fa4ea7bdf49e96bd64d22113ee25c
DevanFarmer <devanf21@gmail.com>
2024-10-20 14:24:57 +0200

    Location Validation

--- Fault Subsystem/FaultSubsystem/FaultSubsystem/Models/Location.cs
+++ Fault Subsystem/FaultSubsystem/FaultSubsystem/Models/Location.cs
@@ -1,11 +1,24 @@
-﻿namespace FaultSubsystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FaultSubsystem.Models
 {
     public class Location
     {
         public int LocationID { get; set; }
+        
+        [Required(ErrorMessage = "Address Line 1 is required")]
+        [StringLength(55, MinimumLength = 3, ErrorMessage = "Address Line 1 must be between 3 and 55 characters")]
         public string AddressLine1 { get; set; }
+        
+        [StringLength(55, MinimumLength = 3, ErrorMessage = "Address Line 1 must be between 3 and 55 characters")]  
         public string? AddressLine2 { get; set; }
+        
+        [Required(ErrorMessage = "City Name is required")]
+        [StringLength(55, MinimumLength = 3, ErrorMessage = "City must be between 3 and 55 characters")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Postal Code is required")]
+        [StringLength(55, MinimumLength = 3, ErrorMessage = "Postal Code must be between 3 and 55 characters")]
         public string PostalCode { get; set; }
     }
 }
