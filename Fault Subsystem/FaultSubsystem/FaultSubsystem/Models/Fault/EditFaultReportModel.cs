5b9b0252a81df3d3f8348370e356150a985c0b76
DevanFarmer <devanf21@gmail.com>
2024-10-20 14:47:51 +0200

    Fault Technician Validation
    Untested

--- Fault Subsystem/FaultSubsystem/FaultSubsystem/Models/Fault/EditFaultReportModel.cs
+++ Fault Subsystem/FaultSubsystem/FaultSubsystem/Models/Fault/EditFaultReportModel.cs
@@ -1,12 +1,24 @@
-﻿namespace FaultSubsystem.Models.Fault
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace FaultSubsystem.Models.Fault
 {
     public class EditFaultReportModel
     {
         public int FaultID { get; set; }
-        public string Diagnosis { get; set; } // Allow employee to input diagnosis
+
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "First Name must be between 3 and 255 characters")]
+        public string? Diagnosis { get; set; } // Allow employee to input diagnosis
+
+        [Required(ErrorMessage = "Please select a Fault Status.")]
         public int FaultStatusID { get; set; } // Store the selected status
+
+        [DataType(DataType.Date, ErrorMessage = "Please enter a valid date.")]
         public DateTime? ScheduledRepairDate { get; set; }
-        public string Notes { get; set; }
+
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "First Name must be between 3 and 255 characters")]
+        public string? Notes { get; set; }
+        
         public List<FaultStatus> AvailableStatuses { get; set; } // Dropdown options
     }
 }
